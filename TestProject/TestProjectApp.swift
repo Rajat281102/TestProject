821c21b0f863d52a141818c86986a83e7ecd609f
Vikas <rajatpilania981@gmail.com>
2023-09-08 16:01:44 +0530

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Babblu Bhaiya on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
